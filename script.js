c1cd09a3ae970c19c393d9a8511766c298fe972e
Mur <murmavilink@gmail.com>
2022-11-26 15:03:28 +0600

    урок 2 (hard) выполнено

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 'use strict';
 
 
+const num = 266219;
+const numArray = num.toString().split('');
 
+const funcNum = function() {
+    let result = 1;
 
+    for(let i = 0; i < numArray.length; i++) {
+        result *= numArray[i];
+    }
+    
+    return result;
+};
+
+console.log( funcNum() );
+
+// const resultPow = funcNum() ** 3;
+// console.log( funcNum() ** 3 );
+// console.log( resultPow.toString().substring(0, 2) );
+
+const result = numArray.reduce(function (sum, number) {
+    return sum *= number;
+}, 1);
+
+console.log(result);
 
 
