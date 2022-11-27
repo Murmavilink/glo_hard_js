c2bad79fbb79c9c9bce596bdd01d7691fe2a4177
Mur <murmavilink@gmail.com>
2022-11-27 19:00:10 +0600

    урок 3 (hard) выполнено

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
 'use strict';
 
+const lang = prompt('Ввидите язык!').toLowerCase();
 
-const num = 266219;
-const numArray = num.toString().split('');
 
-const funcNum = function() {
-    let result = 1;
 
-    for(let i = 0; i < numArray.length; i++) {
-        result *= numArray[i];
-    }
-    
-    return result;
+if(lang == 'ru') {
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else if(lang == 'en') {
+    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+} else {
+    console.log('not');
+}
+
+switch (lang) {
+    case 'ru':
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+        break;
+    case 'en':
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+        break;
+    default:
+        console.log('Непредвиденное значение!');
+}
+
+let langArr = [];
+langArr.ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+langArr.en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log(langArr[lang]);
+
+const langArray = {
+    ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 };
+console.log(langArray[lang]);
 
-console.log( funcNum() );
 
-// const resultPow = funcNum() ** 3;
-// console.log( funcNum() ** 3 );
-// console.log( resultPow.toString().substring(0, 2) );
+const namePerson = 'Александр';
 
-const result = numArray.reduce(function (sum, number) {
-    return sum *= number;
-}, 1);
+console.log( (namePerson === 'Артем') ? 'Директор' : (namePerson === 'Александр') ? 'Преподаватель' : 'Студент' );
 
-console.log(result);
-
+// let namePerson = prompt('Ввдите ваше имя *');
+// let position;
+//  true == Boolean( ((namePerson == 'Артем' ? position = 'Директор' : 0 ) || (namePerson == 'Александр' ? position = 'Преподаватель' : 0)) ? console.log(position) : console.log('Студент'));
 
