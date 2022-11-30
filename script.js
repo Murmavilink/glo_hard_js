978bbc7f8a43488570d514485f1b05eb7d6def05
Mur <murmavilink@gmail.com>
2022-11-30 21:38:29 +0600

    урок 4 (hard) выполнено

--- script.js
+++ script.js
@@ -1,45 +1,17 @@
 'use strict';
 
-const lang = prompt('Ввидите язык!').toLowerCase();
 
+const strHandler = (arg) => {
 
+    if(typeof arg === 'string') {
+        let str = arg.trim();
 
-if(lang == 'ru') {
-    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-} else if(lang == 'en') {
-    console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-} else {
-    console.log('not');
-}
+        return str.length > 15 ? str = str.substring(0, 15) + '...' : str;
+ 
+    } else {
+        console.log('Не строка!');
+    }
 
-switch (lang) {
-    case 'ru':
-        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
-        break;
-    case 'en':
-        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
-        break;
-    default:
-        console.log('Непредвиденное значение!');
-}
-
-let langArr = [];
-langArr.ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-langArr.en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-console.log(langArr[lang]);
-
-const langArray = {
-    ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 };
-console.log(langArray[lang]);
 
-
-const namePerson = 'Александр';
-
-console.log( (namePerson === 'Артем') ? 'Директор' : (namePerson === 'Александр') ? 'Преподаватель' : 'Студент' );
-
-// let namePerson = prompt('Ввдите ваше имя *');
-// let position;
-//  true == Boolean( ((namePerson == 'Артем' ? position = 'Директор' : 0 ) || (namePerson == 'Александр' ? position = 'Преподаватель' : 0)) ? console.log(position) : console.log('Студент'));
-
+console.log( strHandler('Hello my freinds and cat') );
