b59c69b555af9aa030451f6c887a98cdef9e6008
Mur <murmavilink@gmail.com>
2022-12-10 16:09:25 +0600

    урок 10 (hard) выполнено

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
 'use strict';
 
-const weekBox = document.querySelector('.week');
-weekBox.style.fontSize = 25 + 'px';
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const todayDay = new Date();
+const ul = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
+button.addEventListener('click', () => {
 
-
-const addStyle = (weekDay) => {
-    weekDay.style.fontStyle = 'italic';
-    weekDay.style.color = 'red';
-};
-
-
-week.forEach((item, index) => {
-    const weekDay = document.createElement('div');
-
-    weekDay.style.padding = '10px';
-
-    if(todayDay.getDay() - 1 === index) {
-        weekDay.style.fontWeight = 900;
-        weekDay.style.fontSize = 40 + 'px';
-    } 
+    if(input.value) {
+        const li = document.createElement('li');
     
-    
-    item === 'Суббота' || item === 'Воскресенье' ? addStyle(weekDay) : '';
+        li.textContent = input.value;
+        ul.append(li);
+        input.value = '';
+    }
 
 
-    weekDay.textContent = item;
-
-    weekBox.insertAdjacentElement('beforeend', weekDay);
 });
-
