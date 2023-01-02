954e23ea8812a4c4938be4c6dee3c6b7ae0b67dc
Mur <murmavilink@gmail.com>
2023-01-02 16:01:35 +0600

    урок 19 (hard) выполнено

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 'use strict';
 
+// 1
+const input = document.querySelector('input[type=text]');
+const p = document.querySelector('p');
+
+const showText = (value) => {
+    p.textContent = value;
+};
+
+input.addEventListener('input', () => {
+    setTimeout(showText, 300, input.value);
+});
+
+
+// 2
+// const input = document.getElementById('input');
+// const text = document.getElementById('text');
+
+// function debounce(func, timeout) {
+//     return function (args) {
+//         let previousCall = this.lastCall;
+//         this.lastCall = Date.now();
+//         if (previousCall && ((this.lastCall - previousCall) <= timeout)) {
+//             clearTimeout(this.lastCallTimer);
+//         }
+//         this.lastCallTimer = setTimeout(() => func(args), timeout);
+//     };
+// }
+
+// input.addEventListener('input', debounce(() => { text.textContent = input.value; }, 300));
